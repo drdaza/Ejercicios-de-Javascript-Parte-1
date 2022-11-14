193c7c22376a6470e50ad7f8415bee8b4e4c5fe5
David Daza <drdaza18@gmail.com>
2022-11-14 20:38:26 +0100

    added ex-1

---
+++ src/ex-1/app.js
@@ -0,0 +1,22 @@
+/*
+* Crear un programa Escribe un programa que escriba en la pantalla un texto de tipo <h1> que diga “Hello Javascript”.
+* Crear mi archivo html donde va a estár el programa
+++ Crear un archivo css
+++ Vincula mi archivo css a mi archivo html
+++ Añadir un poco de estilo
+* Crear el espacio donde debe ir el texto de tipo <h1>
+* Vincular mi archivo JavaScript con mi archivo html
+* Acceder al espacio donde estará el texto de tipo <h1>
+++ probar si funciona el vinculo del archivo y el acceso al espacio.
+* Crear el texto que voy a introducir en el espacio
+* Asignar ese texto al espacio donde debe estar
+*/
+
+function WriteHelloJavascrit(){
+    let acces = document.getElementById("title");
+    console.log(acces);
+    let suma = 3+5;
+    let message = "<h1>Hello JavaScript</h1> ";
+    acces.innerHTML = message;
+}
+WriteHelloJavascrit();
